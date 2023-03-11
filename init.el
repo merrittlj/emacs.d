0c9690ead31b4ab9ef9f6758dc4459aac56b81f2
Lucas Merritt <lucas.j.merritt@gmail.com>
2023-03-11 12:07:32 -0700

    relative line numbers addition

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 (scroll-bar-mode -1)
 (menu-bar-mode -1);
 (global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)
 (blink-cursor-mode 0)
 (setq-default cursor-type `hbar)
 
