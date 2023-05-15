619ad413b725a3d8b2dfb89faa0ab81a5dff43e6
Lucas Merritt <lucas.j.merritt@gmail.com>
2023-05-14 19:59:14 -0600

    email/gnus support

---
+++ transient/history.el
@@ -0,0 +1,12 @@
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-merge nil)
+ (magit-pull nil)
+ (magit-push nil
+             ("--force")
+             ("--force-with-lease"))
+ (magit-reset nil)
+ (magit-revert
+  ("--edit"))
+ (magit-show-refs nil)
+ (magit-stash nil))
